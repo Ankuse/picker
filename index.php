11c86866f5e6318b36422678ae5a6f431c88ac02
Ankuse <esukna@gmail.com>
2017-04-22 20:34:50 +0300

    Branch dev index lorem test

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ $APPLICATION->SetTitle("Главная");
         <div class="row">
             <section>
                 <h2>Content</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid distinctio labore libero nulla omnis quaerat ratione sunt totam, voluptas? Amet aspernatur culpa eligendi modi perferendis quod sint tempore temporibus voluptates.</p>
             </section>
         </div>
     </div>
